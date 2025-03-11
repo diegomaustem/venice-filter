d6e8a9ad7ba428e0ddf183c2cf3e9ec7162cd9ef
maustem <diego.maustem@gmail.com>
2025-03-11 07:52:54 -0300

    :test_tube: test: Displaying product categories and brands in the  test componente livewire

--- app/Livewire/ProductSearch.php
+++ app/Livewire/ProductSearch.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductSearch extends Component
 {
+
+    public $search = '';
+    public array $selectedCategory = [];
+    public array $selectedBrand = [];
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'selectedCategory' => ['except' => []],
+        'selectedBrand' => ['except' => []],
+    ];
+    
     public function render()
     {
-        return view('livewire.product-search');
+        // Teste consultas ::: 
+        $products = Product::all();
+
+        $categories = Category::all();
+        $brands = Brand::all();
+
+
+        return view('livewire.product-search', compact('products', 'categories', 'brands'));
     }
 }
--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,4 +1,7 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
     <h1>Hi</h1>
+
+    <p>{{ $products }}</p>
+    <p>{{ $categories }}</p>
+    <p>{{ $brands }}</p>
 </div>
